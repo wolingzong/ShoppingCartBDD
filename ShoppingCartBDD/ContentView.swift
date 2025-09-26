f1a0dbe0b194f750e69ce9e3fbe8f73e759a0fe1
watson <276322154@qq.com>
2025-09-27 08:29:49 +0900

    Initial Commit

---
+++ ShoppingCartBDD/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShoppingCartBDD
+//
+//  Created by 沃霊宗 on 2025/09/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShoppingCartBDD/ShoppingCartBDDApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingCartBDDApp.swift
+//  ShoppingCartBDD
+//
+//  Created by 沃霊宗 on 2025/09/27.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingCartBDDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
